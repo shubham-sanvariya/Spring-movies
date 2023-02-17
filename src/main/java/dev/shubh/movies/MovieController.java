726dffe4fe21ab51f0792a5ec30fc53b0cb49ab4
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-17 20:45:06 +0530

    changing List to Optional

--- src/main/java/dev/shubh/movies/MovieController.java
+++ src/main/java/dev/shubh/movies/MovieController.java
@@ -1,13 +1,16 @@
 package dev.shubh.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -20,4 +23,10 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getallMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
+
+    @GetMapping("/{imdbid}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String  imdbid) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbid), HttpStatus.OK);
+    }
+
 }
