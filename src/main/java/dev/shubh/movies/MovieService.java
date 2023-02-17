a0dc77c95bbb74d7be4d9856ae1aa1b466709588
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-17 20:46:10 +0530

    adding property singleMovie

--- src/main/java/dev/shubh/movies/MovieService.java
+++ src/main/java/dev/shubh/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.shubh.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,4 +16,9 @@ public class MovieService {
         System.out.println(movieRepository.findAll().toString());
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> singleMovie(String  imdbid) {
+        return movieRepository.findMovieByImdbId(imdbid);
+    }
+
 }
